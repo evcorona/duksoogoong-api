e238a6b7eb18c90518b9e890f1be908c52ee400e
evcorona <eldav.corona@gmail.com>
2024-09-19 22:24:22 -0600

    Deploy to vercel

--- api/index.js
+++ api/index.js
@@ -1,15 +1,37 @@
 /* eslint-disable no-console */
-require('dotenv').config()
+
+const cors = require('cors')
+const express = require('express')
 
 const db = require('../src/lib/db')
-const server = require('./server')
 
+const authRouter = require('../src/routes/auth')
+const userRouter = require('../src/routes/user')
+const studentRouter = require('../src/routes/student')
+const schoolRouter = require('../src/routes/school')
+
+// Server configuration
+const server = express()
+
+// Server tools
+server.use(cors())
+server.use(express.json())
+
+// Server
+server.use('/auth', authRouter)
+server.use('/users', userRouter)
+server.use('/students', studentRouter)
+server.use('/schools', schoolRouter)
+
+// Server response
+server.get('/', (request, response) => {
+  response.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  response.json({ success: true, message: '🚀 DukSooGoong.api' })
+})
+
+// DB Connect
 db.connect
-  .then(() =>
-    server.listen('8080', () =>
-      console.log('Server DukSooGoong is listening 8080 🚀')
-    )
-  )
-  .catch(error => {
-    console.error('Error: ', error)
-  })
+  .then(() => console.log('Server DukSooGoong is listening 🚀'))
+  .catch(error => console.error('Error: ', error))
+
+module.exports = server
